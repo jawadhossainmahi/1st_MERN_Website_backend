b1503ef2aaa8517feea197d3b36e8fce093af7af
Mahi <jawadhossainmahi@gmail.com>
2023-10-22 23:13:21 +0600

    first Commit in 1st backend branch

--- db.js
+++ db.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 
 const main = async () => {
     await mongoose.connect('mongodb://127.0.0.1:27017/iNoteBook');
-
     // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
